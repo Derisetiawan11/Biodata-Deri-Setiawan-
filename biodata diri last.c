f73fcf70336d4588e1011b57f9aefb932ae791f3
Derisetiawan <deridumen19@gmail.com>
2020-09-22 21:07:06 +0700

    biodata deri

---
+++ biodata diri last.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+	printf("\BIODATA MAHASISWA\N\N");
+	printf("Nama : Deri Setiawan\n");
+	printf("Npm : 19081010013\n");
+	printf("Jurusan : teknik informatika\n");
+	printf("Email : deridumen19@gmail.com\n");
+	printf("Alamat : jl dukuh menanggal no 45 surabaya\n");
+	printf("Ttl : Surabaya\n");
+	printf("Jenis kelamin : laki-laki\n");
+	
+	return 0;	
+}
+	
